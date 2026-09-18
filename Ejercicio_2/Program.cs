88c803f614945da03e5d8d0b5d6b2b2c73e0878b
lozanoalex028-bit <lozanoalex028@gmail.com>
2026-09-18 12:07:47 -0600

    Add files via upload

---
+++ Ejercicio_2/Program.cs
@@ -0,0 +1,48 @@
+﻿// --------------------------------------------------------------------
+// ENCABEZADO DEL PROGRAMA
+
+// Muestra en la consola el nombre o número del ejercicio.
+Console.WriteLine("EJERCICIO_2");
+
+// Muestra el título que describe lo que realiza el programa.
+Console.WriteLine("CÁLCULO DEL PROMEDIO DE TRES CALIFICACIONES");
+
+// Deja una línea en blanco para separar visualmente el encabezado.
+Console.WriteLine();
+
+// --------------------------------------------------------------------
+// ENTRADAS DE DATOS
+
+Console.Write("Ingrese la primera calificación: ");
+
+double calificacion1 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Ingrese la segunda calificación: ");
+
+double calificacion2 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Ingrese la tercera calificación: ");
+
+double calificacion3 = Convert.ToDouble(Console.ReadLine());
+
+
+// --------------------------------------------------------------------
+// OPERACIONES
+
+double promedio = (calificacion1 + calificacion2 + calificacion3) / 3.0;
+
+// --------------------------------------------------------------------
+// SALIDAS DE DATOS
+
+Console.WriteLine();
+
+Console.WriteLine($"El promedio de las tres calificaciones es: {promedio:F2}");
+
+if (promedio >= 70)
+{
+    Console.WriteLine("Felicidades Alumno Aprobado");
+}
+else
+{
+    Console.WriteLine("Alumno Reprobado Sigue Intentandolo");
+}
